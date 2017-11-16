e895f752ef6fea9258a4cfbf1508b72aafef04c9
ivannz <ivannz@yandex.ru>
2017-11-16 17:31:43 +0300

    loader/saver for general objects

---
+++ experiments/utils.py
@@ -0,0 +1,60 @@
+import os
+import time
+import gzip
+import pickle
+
+
+def save(obj, path, gz=None):
+    """Pickle a pythonic `obj` into a file given by `path`.
+
+    Parameters
+    ----------
+    obj: any python object
+        An object to pickle.
+    path: string
+        A file in which to pickle the object.
+    gz: integer, or None
+        If None, then does not apply compression while pickling. Otherwise
+        must be an integer 0-9 which determines the level of GZip compression:
+        the lower the level the less thorough but the more faster the
+        compression is. Value `0` produces a GZip archive with no compression
+        whatsoever, whereas the value of `9` produces the most compressed
+        archive.
+
+    Returns
+    -------
+    filename: string
+        The name of the resulting archive.
+    """
+    if not(gz is None or (isinstance(gz, int) and 0 <= gz <= 9)):
+        raise TypeError("""`gz` parameter must be either `None` """
+                        """or an integer 0-9.""")
+
+    open_ = open if gz is None else lambda f, m: gzip.open(f, m, gz)
+    filename_ = "%s-%s.%s" % (path, time.strftime("%Y%m%d_%H%M%S"),
+                              "pic" if gz is None else "gz")
+
+    with open_(filename_, "wb+") as f:
+        pickle.dump(obj, f)
+    return filename_
+
+
+def load(filename):
+    """Recover an object from the file identified by `filename`.
+
+    Parameters
+    ----------
+    filename: string
+        A `file` in which an object is pickled.
+
+    Returns
+    -------
+    object: a python object
+        The recovered pythonic object.
+    """
+    open_ = open if not filename.endswith(".gz") else gzip.open
+
+    with open_(filename, "rb") as f:
+        obj = pickle.load(f)
+
+    return obj
