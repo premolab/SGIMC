b9038f860f57cf60bdbdcebdd6e41ae26607627f
ivannz <ivan.nazarov@skolkovotech.ru>
2018-03-20 19:51:11 +0300

    FIX: updated value, grad and hess-v calls

--- sgimc/algorithm/misc.py
+++ sgimc/algorithm/misc.py
@@ -7,8 +7,15 @@ def f_valp(x, Obj, D, eta, C_ridge=1.0):
     # Compute the forward pass.
     W = x.reshape(D.shape)
     Obj.forward(W)
-    return Obj.value() + 0.5 * (np.linalg.norm(W - D, ord="fro")**2) / eta \
-                       + 0.5 * C_ridge * np.linalg.norm(W, ord="fro")**2
+
+    valp = Obj.value()
+    if eta > 0:
+        valp += 0.5 * (np.linalg.norm(W - D, ord="fro")**2) / eta
+
+    if C_ridge > 0:
+        valp += 0.5 * C_ridge * np.linalg.norm(W, ord="fro")**2
+
+    return valp
 
 
 def f_grad(x, Obj, D, eta, C_ridge=1.0):
@@ -16,13 +23,28 @@ def f_grad(x, Obj, D, eta, C_ridge=1.0):
     # Update grad and hess statistics.
     W = x.reshape(D.shape)
     Obj.backward()
-    return (Obj.grad() + (W - D) / eta + C_ridge * W).reshape(-1)
+
+    grad = Obj.grad()
+    if eta > 0:
+        grad += (W - D) / eta
+
+    if C_ridge > 0:
+        grad += C_ridge * W
+
+    return grad.reshape(-1)
 
 
 def f_hess(v, Obj, D, eta, C_ridge=1.0):
     """Compute the hessian-vector product for Obj."""
-    mult = (C_ridge * eta + 1) / eta
-    return Obj.hess_v(v.reshape(D.shape)).reshape(-1) + v * mult
+    W = v.reshape(D.shape)
+    hess_v = Obj.hess_v(W)
+    if eta > 0:
+        hess_v += W / eta
+
+    if C_ridge > 0:
+        hess_v += C_ridge * W
+
+    return hess_v.reshape(-1)
 
 
 def f_fused(x, Obj, D, eta, C_ridge=1.0):
@@ -34,12 +56,14 @@ def f_fused(x, Obj, D, eta, C_ridge=1.0):
     # Update grad and hess statistics.
     Obj.backward()
 
-    delta = W - D
-    valp = Obj.value() + 0.5 * (np.linalg.norm(delta, ord="fro")**2) / eta
-    grad = Obj.grad() + delta / eta
+    valp, grad = Obj.value(), Obj.grad()
+    if eta > 0:
+        delta = W - D
+        valp += 0.5 * (np.linalg.norm(delta, ord="fro")**2) / eta
+        grad += delta / eta
 
     if C_ridge > 0:
-        valp += 0.5 * C_ridge * np.linalg.norm(W, ord="fro")**2
+        valp += 0.5 * C_ridge * (np.linalg.norm(W, ord="fro")**2)
         grad += C_ridge * W
 
     return valp, grad.reshape(-1)
