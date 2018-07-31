c475ac8f806408cfe22777ed8fc4ea50df875c55
Boris <bg.dustpelt@gmail.com>
2018-07-31 12:03:38 +0300

    removed old n_features experiment and cleaned up useless utility functions

--- experiments/utils.py
+++ experiments/utils.py
@@ -1,35 +1,7 @@
 """Various utility functions and classes to help with experimentation."""
-import signal
-
 import numpy as np
-from scipy.sparse import coo_matrix
-from sklearn.metrics import mean_squared_error
 
 from sgimc.utils import sparsify_with_mask
-
-
-class DelayedKeyboardInterrupt(object):
-    """An uninterruptible critical section.
-
-    This critical section postpones the firing on the keyboard interrupt
-    unitl after the its `with`-scope.
-    """
-
-    def __enter__(self):
-        """Enter the critical section and hook the keyboard interrupts."""
-        self.signal_received = False
-        self.old_handler = signal.getsignal(signal.SIGINT)
-        signal.signal(signal.SIGINT, self.handler)
-
-    def handler(self, sig, frame):
-        """Handle the fired interrupt."""
-        self.signal_received = (sig, frame)
-
-    def __exit__(self, type, value, traceback):
-        """Leave the scope of the critical section and service interruprts."""
-        signal.signal(signal.SIGINT, self.old_handler)
-        if self.signal_received:
-            self.old_handler(*self.signal_received)
 
 
 def add_noise_features(X, n_features, scale, random_state, return_sparse=True):
@@ -45,26 +17,3 @@ def add_noise_features(X, n_features, scale, random_state, return_sparse=True):
         X_comb = sparsify_with_mask(X_comb, X_comb != 0)
         
     return X_comb
-
-
-# =============================== Functions to calculate loss ===============================
-
-def norm(r, mask=None):
-    if mask is None: return np.sum(r**2)
-    else: return np.sum(r[mask]**2)
-    
-
-def relative_loss(r, r_hat, mask=None):
-    a = norm((r - r_hat), mask=mask)
-    b = norm(r, mask=mask)
-    return a/b
-
-
-def calculate_loss(R, X, W_stack, H_stack, Y, mask=None):
-    """Calculates loss specified in 'norm' fucntion between R and R_hat,
-    given by the model R_hat = X W H' Y'. Mask provides element wise loss."""
-    
-    r_hat = get_prediction(X, W_stack, H_stack, Y)
-    l = relative_loss(R, r_hat, mask=mask)
-    
-    return l
